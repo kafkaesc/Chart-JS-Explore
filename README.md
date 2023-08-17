f3f3f5a497ed45965ff1aff54da37a9cf4ade7ba
kafkaesc <jared.a.hettinger@gmail.com>
2023-08-17 10:51:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Chart.js Explore
+
+Built by Jared Hettinger
+
+A simple project for exploring [Chart.js](https://www.chartjs.org).
+
+## Initial Run
+
+1. Navigate to the project folder in the terminal
+1. Run `npm install`
+1. Run the Tailwind watcher `npx tailwindcss -i ./src/input.css -o ./src/output.css --watch`
+1. Run the app `npm start`
+
+## Run After Install
+
+1. Navigate to the project folder in the terminal
+1. Run the Tailwind watcher `npx tailwindcss -i ./src/input.css -o ./src/output.css --watch`
+1. Run the app `npm start`
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import About from './pages/About';
+import App from './App';
+import Error from './Error';
+import Home from './pages/Home';
+
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		errorElement: <Error />,
+		children: [
+			{ path: '/', element: <Home /> },
+			{ path: '/About', element: <About /> },
+			{ path: '/Home', element: <Home /> },
+		],
+	},
+]);
+root.render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
