53b1c72afc831b8226b6fb2d81adcd6782c1e3c0
kafkaesc <jared.a.hettinger@gmail.com>
2023-08-28 17:48:03 -0500

    Utlizing H1 component

--- src/Error.jsx
+++ src/Error.jsx
@@ -1,3 +1,4 @@
+import H1 from './elements/H1';
 export default function Error() {
-	return <h1 className="text-3xl font-bold text-center">Error Component</h1>;
+	return <H1 className="text-3xl font-bold text-center">Error Component</H1>;
 }
