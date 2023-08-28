6ada2f03293430610c518b8053bbd71a2b4b441a
kafkaesc <jared.a.hettinger@gmail.com>
2023-08-28 17:45:36 -0500

    Adding className prop to the Button component

--- src/elements/Button.jsx
+++ src/elements/Button.jsx
@@ -1,5 +1,9 @@
-export default function Button({ children, ...props }) {
-	return (
+export default function Button({ children, className, ...props }) {
+	return className ? (
+		<button {...props} className={`px-2 px-4 border ${className}`}>
+			{children}
+		</button>
+	) : (
 		<button {...props} className="px-2 px-4 border">
 			{children}
 		</button>
