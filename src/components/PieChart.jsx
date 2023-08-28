8aa44f20efb900006a69d56bd59bcb3242831579
kafkaesc <jared.a.hettinger@gmail.com>
2023-08-28 17:50:38 -0500

    Utlizing H2 component in BarChart and PieChart

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import { Pie } from 'react-chartjs-2';
+import H2 from '../elements/H2';
 
 export default function PieChart({ chartData, chartTitle, h2Title }) {
 	return (
 		<div className="max-w-2xl mx-auto">
-			{h2Title && <h2 className="text-xl font-bold text-center">{h2Title}</h2>}
+			{h2Title && <H2 className="text-xl font-bold text-center">{h2Title}</H2>}
 			<Pie
 				data={chartData}
 				options={{
