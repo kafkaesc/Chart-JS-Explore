160a359900f14bd15bc65e6727b329d980fe1e8e
kafkaesc <jared.a.hettinger@gmail.com>
2023-09-01 10:20:55 -0500

    Updating line chart and dynamic pages

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
 import { Line } from 'react-chartjs-2';
+import H2 from '../elements/H2';
 
-export default function LineChart({ chartData, chartTitle, h2Title }) {
+export default function LineChart({
+	chartData,
+	chartTitle,
+	h2Title,
+	...props
+}) {
 	return (
 		<div className="max-w-2xl mx-auto">
-			{h2Title && <h2 className="text-xl font-bold text-center">{h2Title}</h2>}
+			{h2Title && <H2 className="text-xl font-bold text-center">{h2Title}</H2>}
 			<Line
 				data={chartData}
 				options={{
-					plugins: {
-						legend: { display: false },
-						title: { display: true, text: chartTitle ? chartTitle : '' },
-					},
+					plugins: props.plugins
+						? props.plugins
+						: {
+								legend: { display: false },
+								title: {
+									display: true,
+									text: chartTitle ? chartTitle : '',
+								},
+						  },
+					responsive: true,
+					scales: props.scales ? props.scales : undefined,
 				}}
 				redraw={true}
 			/>
--- src/pages/Dynamic.jsx
+++ src/pages/Dynamic.jsx
@@ -1,35 +1,24 @@
 import { useState } from 'react';
 import Button from '../elements/Button';
+import H1 from '../elements/H1';
 import LineChart from '../components/LineChart';
+import Navigation from '../components/Navigation';
 
-const initialValues = [
-	{ id: 1, value: 8 },
-	{ id: 2, value: 6 },
-	{ id: 3, value: 7 },
-	{ id: 4, value: 5 },
-	{ id: 5, value: 3 },
-];
+import { jenny as initialValues } from '../utils/Data';
+
+const chartScales = { y: { max: 12, min: 0 } };
 
 export default function Dynamic() {
 	const [chartData, setChartData] = useState({
 		labels: initialValues.map((item, index) => index + 1),
 		datasets: [
 			{
-				lineTension: 0.2,
+				//lineTension: 0.2,
 				data: initialValues.map((item) => item.value),
 				borderWidth: 3,
 			},
 		],
 	});
-
-	function shiftChartData() {
-		if (chartData.labels.length > 0) {
-			const newChartData = { ...chartData };
-			newChartData.datasets[0].data.shift();
-			newChartData.labels.shift();
-			setChartData(newChartData);
-		}
-	}
 
 	function pushChartData() {
 		const newChartData = { ...chartData };
@@ -44,12 +33,44 @@ export default function Dynamic() {
 		setChartData(newChartData);
 	}
 
+	function resetChartData() {
+		setChartData({
+			labels: initialValues.map((item, index) => index + 1),
+			datasets: [
+				{
+					data: initialValues.map((item) => item.value),
+					borderWidth: 3,
+				},
+			],
+		});
+	}
+
+	function shiftChartData() {
+		if (chartData.labels.length > 0) {
+			const newChartData = { ...chartData };
+			newChartData.datasets[0].data.shift();
+			newChartData.labels = [];
+			newChartData.datasets[0].data.forEach((datum, index) => {
+				newChartData.labels.push(index + 1);
+			});
+			setChartData(newChartData);
+		}
+	}
+
 	return (
 		<>
-			<h1 className="text-3xl font-bold text-center">Dynamic Line Chart</h1>
-			<LineChart chartData={chartData} h2Title={'Rando Dato'} />
-			<Button onClick={pushChartData}>Add</Button>
-			<Button onClick={shiftChartData}>Remove</Button>
+			<Navigation />
+			<H1 className="text-3xl font-bold text-center">Dynamic Line Chart</H1>
+			<LineChart
+				chartData={chartData}
+				h2Title={'Rando Dato'}
+				scales={chartScales}
+			/>
+			<div className="text-center">
+				<Button onClick={pushChartData}>Add</Button>
+				<Button onClick={shiftChartData}>Remove</Button>
+				<Button onClick={resetChartData}>Reset</Button>
+			</div>
 		</>
 	);
 }
