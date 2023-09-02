cfae1c95ce3ac0155dac2021b9507dc6fdf843e9
kafkaesc <jared.a.hettinger@gmail.com>
2023-09-01 19:40:35 -0500

    Breaking off flex graph styles into CSS

--- src/pages/Flex.jsx
+++ src/pages/Flex.jsx
@@ -1,5 +1,6 @@
 import H1 from '../elements/H1';
 import Navigation from '../components/Navigation';
+import './Flex.css';
 
 const tailWidth = 50;
 const tickWidth = 25;
@@ -11,63 +12,33 @@ export default function Flex() {
 		<>
 			<Navigation />
 			<H1 className="text-3xl font-bold text-center">Flex Graph</H1>
-			<div style={{ margin: '0 auto', maxWidth: 750 }}>
-				<div style={{ display: 'flex', marginBottom: '2px' }}>
+			<div className="flex-graph-page">
+				<div className="fbg-row">
 					<div style={{ flex: '0 0 ' + tickWidth + 'px' }}>#1</div>
-					<p
-						style={{
-							backgroundColor: '#ef476f',
-							flex: 'grow',
-							paddingLeft: '4px',
-							paddingRight: '4px',
-						}}
-					>
+					<p className="fbg-bar fbg-bar-bg-0">
 						The box in question was, as I say, oblong. It was about six feet in
 						length by two and a half in breadth;
 					</p>
 					<div style={{ flex: '0 0 ' + tailWidth + 'px' }}>(33.3%)</div>
 				</div>
-				<div style={{ display: 'flex', marginBottom: '2px' }}>
+				<div className="fbg-row">
 					<div style={{ flex: '0 0 ' + tickWidth + 'px' }}>#2</div>
-					<p
-						style={{
-							backgroundColor: '#06d6a0',
-							flex: 'grow',
-							paddingLeft: '4px',
-							paddingRight: '4px',
-						}}
-					>
+					<p className="fbg-bar fbg-bar-bg-1">
 						Call me Ishmael. Some years ago—never mind how long precisely—having
 						little or no money in my purse
 					</p>
 					<div style={{ flex: '0 0 ' + tailWidth + 'px' }}>(25.0%)</div>
 				</div>
-				<div style={{ display: 'flex', marginBottom: '2px' }}>
+				<div className="fbg-row">
 					<div style={{ flex: '0 0 ' + tickWidth + 'px' }}>#3</div>
-					<p
-						style={{
-							backgroundColor: '#e85d04',
-							flex: 'grow',
-							paddingLeft: '4px',
-							paddingRight: '4px',
-						}}
-					>
+					<p className="fbg-bar fbg-bar-bg-2">
 						As Gregor Samsa awoke one morning
 					</p>
 					<div style={{ flex: '0 0 ' + tailWidth + 'px' }}>(16.0%)</div>
 				</div>
-				<div style={{ display: 'flex', marginBottom: '2px' }}>
+				<div className="fbg-row">
 					<div style={{ flex: '0 0 ' + tickWidth + 'px' }}>#4</div>
-					<p
-						style={{
-							backgroundColor: '#415a77',
-							flex: 'grow',
-							paddingLeft: '4px',
-							paddingRight: '4px',
-						}}
-					>
-						hello world
-					</p>
+					<p className="fbg-bar fbg-bar-bg-3">hello world</p>
 					<div style={{ flex: '0 0 ' + tailWidth + 'px' }}>(12.7%)</div>
 				</div>
 			</div>
