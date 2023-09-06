b3b7143d827e7b21f707f7a4d09e51ca3dfc8f42
kafkaesc <jared.a.hettinger@gmail.com>
2023-09-05 20:58:48 -0500

    YogaBarChart with the option to go left-to-righ as well

--- src/components/YogaBarChart.jsx
+++ src/components/YogaBarChart.jsx
@@ -2,21 +2,49 @@ import './YogaBarChart.css';
 
 const tickWidth = 25;
 
-export default function YogaBarChart({ chartData, props }) {
-	return (
-		<div className="yoga-bar-chart">
-			{chartData.map((datum, index) => {
-				return (
-					<div className="ybc-row">
-						<div style={{ flex: '0 0 ' + tickWidth + 'px' }}>{datum.label}</div>
+export default function YogaBarChart({ chartData, ...props }) {
+	let ybcJsx = null;
+	if (!props.direction || props.direction === 'right') {
+		ybcJsx = (
+			<div className="yoga-bar-chart">
+				{chartData.map((datum, index) => {
+					return (
+						<div className="ybc-row" key={`ybc-row-${datum.label}`}>
+							<div style={{ flex: '0 0 ' + tickWidth + 'px' }}>
+								{datum.label}
+							</div>
+							<div
+								className={`ybc-bar ybc-bar-bg-${index}`}
+								style={{ width: datum.value + '%' }}
+							></div>
+							<div className="ybc-default-tail">{datum.tailLabel}</div>
+						</div>
+					);
+				})}
+			</div>
+		);
+	} else if (props.direction === 'left') {
+		ybcJsx = (
+			<div className="yoga-bar-chart">
+				{chartData.map((datum, index) => {
+					return (
 						<div
-							className={`ybc-bar ybc-bar-bg-${index}`}
-							style={{ width: datum.value + '%' }}
-						></div>
-						<div className="ybc-default-tail">{datum.tailLabel}</div>
-					</div>
-				);
-			})}
-		</div>
-	);
+							className="ybc-row ybc-row-rtl"
+							key={`ybc-row-${datum.label}`}
+						>
+							<div style={{ flex: '0 0 ' + tickWidth + 'px' }}>
+								{datum.label}
+							</div>
+							<div
+								className={`ybc-bar ybc-bar-bg-${index}`}
+								style={{ width: datum.value + '%' }}
+							></div>
+							<div className="ybc-default-tail">{datum.tailLabel}</div>
+						</div>
+					);
+				})}
+			</div>
+		);
+	}
+	return ybcJsx;
 }
