d3d682b90b06e66e4e51a8b42cb6376a6b582b71
kafkaesc <jared.a.hettinger@gmail.com>
2023-10-31 13:12:52 -0500

    Adding gatekeeping to LiveLineChart

--- src/components/LiveLineChart.jsx
+++ src/components/LiveLineChart.jsx
@@ -7,6 +7,22 @@ export default function LiveLineChart({
 	h2Title,
 	...props
 }) {
+	if (!chartData) {
+		console.error('No chartData argument passed to LiveLineChart');
+		return <></>;
+	}
+	if (!chartData.labels) {
+		console.error(
+			'No labels attribute on the chartData argument in LiveLineChart'
+		);
+		return <></>;
+	}
+	if (!chartData.datasets) {
+		console.error(
+			'No datasets attribute on the chartData argument in LiveLineChart'
+		);
+		return <></>;
+	}
 	return (
 		<div className="max-w-2xl mx-auto">
 			{h2Title && <H2 className="text-xl font-bold text-center">{h2Title}</H2>}
